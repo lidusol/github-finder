94cb8273c264e9281000151bf1cf751bca0efb0e
Lydia Solomon <lydiasolomon97@gmail.com>
2022-10-08 23:46:33 +0300

    Create alert.

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
 
 function UserSearch() {
   const [text, setText] = useState("");
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => setText(e.target.value);
 
@@ -11,7 +13,7 @@ function UserSearch() {
     e.preventDefault();
 
     if (text === "") {
-      alert("Please enter something");
+      setAlert("Please enter something", "error");
     } else {
       searchUsers(text);
       setText("");
